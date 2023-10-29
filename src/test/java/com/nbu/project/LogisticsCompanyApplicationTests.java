42df784f3ab5c2fc4471439b226778c8aef0cddc
iGalEvomA <gabitopuzova@gmail.com>
2023-10-29 18:20:17 +0200

    initialise project

---
+++ src/test/java/com/nbu/project/LogisticsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
