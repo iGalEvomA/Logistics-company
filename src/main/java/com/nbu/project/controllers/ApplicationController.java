7efa330d730c66dce583388cda235605057edcc7
Viktor Terziev <viktor.terziev@ocado.com>
2023-11-03 15:37:58 +0200

    Redirect to /login when not logged it

--- src/main/java/com/nbu/project/controllers/ApplicationController.java
+++ src/main/java/com/nbu/project/controllers/ApplicationController.java
@@ -1,5 +1,7 @@
 package com.nbu.project.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -10,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationController {
 
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal @NotNull OAuth2User principal) {
+    public String home(@AuthenticationPrincipal @NotNull OAuth2User principal, HttpServletResponse httpServletResponse) {
         if (principal == null) {
-            // TODO: Redirect to login page
-            return "Hello World !";
+            httpServletResponse.setHeader("Location", "/login");
+            httpServletResponse.setStatus(302);
+            return "Login first";
         }
         return "Hello " + principal.getAttribute("name") + " !";
     }
