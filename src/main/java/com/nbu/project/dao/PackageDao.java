a479272d45d13bb5f6d05c16b87a4bdfcf1d0501
TCherkezov <tefi_4erkezov@abv.bg>
2023-11-19 14:51:26 +0200

    package-controller

---
+++ src/main/java/com/nbu/project/dao/PackageDao.java
@@ -0,0 +1,16 @@
+package com.nbu.project.dao;
+import com.nbu.project.entities.Package;
+
+import java.util.List;
+
+public interface PackageDao {
+
+    void save(Package pkg);
+
+    Package getById(int id);
+    List<Package> getAll();
+
+    void update(Package pkg);
+
+    void deleteById(int id);
+}
