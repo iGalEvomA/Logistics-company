7a85434d7af41412e5b0532ea08025dd235fa95f
Viktor Terziev <terzievviktor@gmail.com>
2024-02-04 17:32:57 +0200

    Added google authentication (#5)
    * Added google authentication
    
    ---------
    
    Co-authored-by: Viktor Terziev <viktor.terziev@ocado.com>

--- src/main/java/com/nbu/project/config/SecurityConfig.java
+++ src/main/java/com/nbu/project/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
