9bbea56346d49d82daa631c28c4c103896ff4ffb
terziev-viktor <terzievviktor@gmail.com>
2024-02-06 22:57:37 +0200

    Fix mock data seed

---
+++ src/main/resources/db/changelog/changes/006-insert-mock-employees.sql
@@ -0,0 +1,8 @@
+INSERT INTO logistics_company.employee (email, name, type, office_address_id, token1, token2) VALUES
+    ('petarpetrov1@mail.com', 'Petar', 'office', 1, 'token1', 'token2'),
+    ('ivanivanov@mail.com', 'Ivan', 'office', 1, 'token1', 'token2'),
+    ('maria@mail.com', 'Maria', 'office', 4, 'token1', 'token2'),
+    ('pencho@mail.com', 'Pencho', 'courier', 4, 'token1', 'token2'),
+    ('mincho@mail.com', 'Mincho', 'courier', 1, 'token1', 'token2'),
+    ('stoyan@mail.com', 'Stoyan', 'courier', 1, 'token1', 'token2'),
+    ('marin@mail.com', 'Marin', 'courier', 1, 'token1', 'token2');
---
+++ src/main/resources/db/changelog/changes/007-insert-mock-customers.sql
@@ -0,0 +1,7 @@
+INSERT INTO logistics_company.customer (email, token1, token2) VALUES
+    ('petya@mail.com', 'token1', 'token2'),
+    ('minka@mail.com', 'token1', 'token2'),
+    ('stoyanka@mail.com', 'token1', 'token2'),
+    ('yasen@mail.com', 'token1', 'token2'),
+    ('martin@mail.com', 'token1', 'token2'),
+    ('lyubo@mail.com', 'token1', 'token2');
